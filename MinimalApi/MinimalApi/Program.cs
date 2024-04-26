1dd23dce644fdec65cf990d63b5820239b130f2c
Serhii Sudakov <serhiisudakov@gmail.com>
2024-04-26 22:02:27 +0300

    added mock for tests api

--- MinimalApi/MinimalApi/Program.cs
+++ MinimalApi/MinimalApi/Program.cs
@@ -6,7 +6,25 @@ var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
+var hotels = new List<Hotel>();
+
+app.MapGet("/hotels", () => hotels);
+app.MapGet("/hotels/{id}", (int id) => hotels.FirstOrDefault(h => h.Id == id));
+app.MapPost("/hotels", (Hotel hotel) => hotels.Add(hotel));
+app.MapPut("/hotels", (Hotel hotel) => {
+    var index = hotels.FindIndex(h => h.Id == hotel.Id);
+    if (index < 0)
+    {
+        throw new Exception("Not found");
+    }
+    hotels[index] = hotel;
+});
+app.MapDelete("/hotels/{id}", (int id) => {
+    var index = hotels.FindIndex(h => h.Id == id);
+    if (index < 0) throw new Exception("Not found");
+    hotels.RemoveAt(index);
+});
 
 var summaries = new[]
 {
@@ -31,4 +49,12 @@ app.Run();
 internal record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
 {
     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
+
+public class Hotel
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public double Latitude { get; set; }
+    public double Longitude { get; set; }
 }
